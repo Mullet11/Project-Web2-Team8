83fe3f68436428ae9341e6cf5e98367ab87a00f4
Rakha <rakha1140athallah@gmail.com>
2026-06-17 17:07:39 +0800

    feat: implement database schema, models and seeders

---
+++ database/migrations/2026_06_17_085635_create_reservations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->string('booking_code')->unique();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('room_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->string('purpose')->nullable();
+            $table->date('reservation_date');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->enum('status', ['active', 'cancelled', 'finished'])->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
